3f3a65b73019a30d94088e877bb43cc11c46a26c
Yaroslav <yarmoshyk96@gmail.com>
2025-02-15 22:33:26 +0200

    Add style to object solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,17 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  return sourceString
+    .split(';')
+    .filter((property) => property.trim().length > 0)
+    .map((property) => getObjectField(property))
+    .reduce((prev, next) => ({ ...prev, ...next }), {});
+}
+
+function getObjectField(property) {
+  return property
+    .split(':')
+    .reduce((prev, next) => ({ [prev.trim()]: next.trim() }));
 }
 
 module.exports = convertToObject;
